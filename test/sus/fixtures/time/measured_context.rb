2827aa83306116973573535ae52fb99a2d33860b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-07-23 16:49:28 +1200

    Fix method names.

--- test/sus/fixtures/time/measured_context.rb
+++ test/sus/fixtures/time/measured_context.rb
@@ -9,14 +9,14 @@ describe Sus::Fixtures::Time::MeasuredContext do
 	include subject
 	
 	it "should measure faster than" do
-		expect{sleep(0.01)}.to have_duration(measured_faster_than(0.0))
+		expect{sleep(0.01)}.to have_duration(be_measured_faster_than(0.0))
 	end
 	
 	it "should measure slower than" do
-		expect{sleep(0.01)}.to have_duration(measured_slower_than(0.02))
+		expect{sleep(0.01)}.to have_duration(be_measured_slower_than(0.02))
 	end
 	
 	it "should measure within" do
-		expect{sleep(0.01)}.to have_duration(measured_within(0.01))
+		expect{sleep(0.01)}.to have_duration(be_measured_within(0.01))
 	end
 end
